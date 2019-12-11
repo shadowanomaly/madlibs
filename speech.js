2b8b950e260ce82e4ddf8a5b3c3461301cef3a51
shadowanomaly <56459683+shadowanomaly@users.noreply.github.com>
2019-12-11 18:48:49 -0500

    Update speech.js

--- speech.js
+++ speech.js
@@ -1,3 +1,9 @@
-var msg = new SpeechSynthesisUtterance();
-let voices =[];
-var voices
+// Init SpeechSynth API
+const synth = window.speechSynthesis;
+
+// DOM Elements
+const noun = document.querySelector('noun');
+const adjective = document.querySelector('adjective');
+const vehicle = document.querySelector('vehicle');
+const  = document.querySelector('#rate');
+const body = document.querySelector('body');

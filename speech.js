0758f7bb187ce63ff75814b203eabc7c81035872
shadowanomaly <56459683+shadowanomaly@users.noreply.github.com>
2019-12-11 18:30:54 -0500

    Update speech.js

--- speech.js
+++ speech.js
@@ -1,16 +1,3 @@
 var msg = new SpeechSynthesisUtterance();
-var voices = window.speechSynthesis.getVoices();
-msg.voice = voices[10]; // Note: some voices don't support altering params
-msg.voiceURI = 'native';
-msg.volume = 1; // 0 to 1
-msg.rate = 1; // 0.1 to 10
-msg.pitch = 2; //0 to 2
-msg.text = 'Hello World';
-msg.lang = 'en-US';
-
-msg.onend = function(e) {
-  console.log('Finished in ' + event.elapsedTime + ' seconds.');
-};
-
-speechSynthesis.speak(msg);
-
+let voices =[];
+var voices
